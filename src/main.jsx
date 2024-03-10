e28df7b8daef00e016d9ec55052eec6774b02fa0
wpmohammad1 <alimohammadjhd@gmail.com>
2024-03-10 23:51:05 +0600

    file setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {
+  RouterProvider,
+} from "react-router-dom";
+import { store } from './Redux/Store';
+import { Provider } from 'react-dedux';
+import router from './Router/routers';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+  <Provider store={store}>
+      <RouterProvider router={router}></RouterProvider>
+    </Provider>
   </React.StrictMode>,
 )
