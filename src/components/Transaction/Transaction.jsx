5ec6d61f8246fed9986a0ca91bea65451ed3cdd7
wpmohammad1 <alimohammadjhd@gmail.com>
2024-03-12 03:28:27 +0600

    create list transaction data

--- src/components/Transaction/Transaction.jsx
+++ src/components/Transaction/Transaction.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { FaEdit } from "react-icons/fa";
 import { FaTrashAlt } from "react-icons/fa";
 
-const Transaction = () => {
+const Transaction = ({transaction}) => {
+    const {name, amount, type} = transaction;
     return (
-        <li className="transaction income">
-            <p>Earned this month</p>
+        <li className={`transaction ${type}`}>
+            <p>{name}</p>
             <div className="right">
-                <p>৳ 100</p>
+                <p>{amount}</p>
                 <button className="link">
                     <FaEdit/>
                 </button>
--- src/components/Transaction/Transactions.jsx
+++ src/components/Transaction/Transactions.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import Transaction from './Transaction';
+import { useSelector } from 'react-redux';
 
 const Transactions = () => {
+    const {isLoading, isError, error, transactions} = useSelector(state => state.transaction);
+
+    let content = null;
+    if(isLoading) content = <p>Loading</p>;
+
+    if(!isLoading && isError) content = <p className='error'>There was an error occured</p>
+    if(!isLoading && !isError && transactions?.length > 0){
+        content = transactions.map( transaction => <Transaction key={transaction.id} transaction={transaction}/>)    
+    }
+    if(!isLoading && !isError && transactions?.length === 0){
+        content = <p>No Transactions Found!</p>   
+    }
+
     return (
         <>
         <p className="second_heading">Your Transactions:</p>
 
         <div className="conatiner_of_list_of_transactions">
             <ul>
-                <Transaction />
+               {content}
             </ul>
         </div>
     </>
